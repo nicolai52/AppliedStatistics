b319ed6e8c0edd8f61bf38e7330f23d0d51871a5
nicolai52 <59474093+nicolai52@users.noreply.github.com>
2024-12-08 12:29:57 +0100

    updating gravitation

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import numpy as np
 times = np.load('pendul_tider/times_LWN.npy')
 
 first_top = (times[:-2] + times[1:-1] + times[2:])[::3]
-middel = (times[1:-1] + times[2:] + times[3:])[::3]
+middel = (times[1:-1] + times[2:] + times[3:])[1::3]
 last_top = (times[2:] + times[3:] + times[4:])[::3]
 
